af0687cbe4d1864c34c21c11e3f9f6c064c9d43e
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-28 17:49:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School 
+  
+  attr_reader :name, :roster 
+  
+  
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+    @roster[grade] = [] if @roster[grade] == nil 
+    
+    @roster[grade] << student_name
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    @roster.sort.each do |grade, hash|
+      @roster[grade] = hash.sort 
+    end 
+    @roster
+    
+  end 
+end 
